e549d8326c7d779983cb054e52223a0bd36c2125
AmanUllah687 <Amanch687@gmail.com>
2024-02-17 16:10:34 +0500

    added some code in JSarrays file

--- Javascript Practice/JSarrays.js
+++ Javascript Practice/JSarrays.js
@@ -18,7 +18,7 @@ console.log(fruits1.copyWithin(2,1,2));
 */
 
 //Entries function
-//entrie() function is itterator object which return key value pairs of an array
+//entries() function is itterator object which return key value pairs of an array
 // It does not  change the orginal Array
 /*
 const cars = ["Honda", "Toyota", "Suzuki" , "Jaguar", "Tesla"];
@@ -29,9 +29,42 @@ for (let e of itterator) {
 */
 
 // Fill Function
-// fill function functions fill the specified elements in an array with static value
+// fill() function functions fill the specified elements in an array with static value
 // you have to specify the position if not specified it can fill all elements and 
 // it ruturns changed array
-
+/*
 const array1 = [1,2,3,4]
 console.log(array1.fill(0,2,3));
+*/
+
+// Filter function
+// filter() function create anew array filled with existing arrsy elements that pases a  test 
+// filter does not excute a function for empty array elements
+// it does not chnge the original array
+/*
+const words = ["Spray", "limit", "Elite", "Exuburant", "destruction", "present"];
+const result = words.filter(words => words.length>6);
+console.log(result);
+*/
+
+// Find function 
+// find() function  returns the value of array element that pasess a test
+// The function excutes once for each element in the array if it find a element where 
+// functions return a true value it return the vlaue of that elementand does not checked the remaining values
+// otherwise it returns undefined
+/*
+const array1 = [5,12,18,130,44]
+let found = array1.find(element => element > 10);
+console.log(found);
+*/
+
+
+// Map Function
+//map() function returns a new  array as a result of performing a given function on each array
+// element .it does not excutes the function for empty elements. it does not change the original array
+/*
+const array1 = [1,4,9,16];
+let result = array1.map(element => element*2);
+console.log(result);
+*/
+
