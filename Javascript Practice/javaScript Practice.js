59602fb30a9e653612e7aae1bd6f101fd2f5f30b
AmanUllah687 <Amanch687@gmail.com>
2024-02-22 11:58:50 +0500

    Added Eular Project Problems file

--- Javascript Practice/javaScript Practice.js
+++ Javascript Practice/javaScript Practice.js
@@ -134,6 +134,11 @@ for (j=10; j<101; j +=10) {
     console.log(j);
 }
 */
-for (let i=3; i<101; i += 2) {
-    console.log(i);
+let sum = 0;
+for (i=1; i<1001; i++) {
+   if(i%3 === 0 || i%5=== 0) {
+    sum += i;
+   }
+
 }
+console.log(sum);
