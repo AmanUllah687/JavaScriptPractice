2bcbdd336bf69893d42818615b305ad2b8575783
AmanUllah687 <Amanch687@gmail.com>
2024-02-23 15:57:33 +0500

    added code

--- Javascript Practice/ProjectEularProblems.js
+++ Javascript Practice/ProjectEularProblems.js
@@ -1,5 +1,7 @@
+// Project Eular Problem 1
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000
+/*
 let sum = 0;
 for (i=1; i<1000; i++) {
    if(i%3 === 0 || i%5=== 0) {
@@ -8,3 +10,26 @@ for (i=1; i<1000; i++) {
 
 }
 console.log(sum);
+*/
+
+// Project Eular Problem 2
+let fibonacciSeries = [0, 1];
+function generateFibonacci(n) {
+   
+
+   for (let i = 2; i < 10; i++) {
+       fibonacciSeries[i] = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+       let sum = 0;
+       if( num % 2 === 0) {
+         sum += num;
+      }
+   }
+   return fibonacciSeries;
+}
+function sumOfEvenNumbers(fibonacciSeries) {
+   let sum = 0;
+   for(let num of result) {
+      
+   return sum;
+}
+console.log(sumOfEvenNumbers());
