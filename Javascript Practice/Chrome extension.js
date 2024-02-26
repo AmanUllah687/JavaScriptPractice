236500791b47728f9a0a90af667735b580fd4380
AmanUllah687 <Amanch687@gmail.com>
2024-02-26 21:17:07 +0500

    added new features in chrome extension file

--- Javascript Practice/Chrome extension.js
+++ Javascript Practice/Chrome extension.js
@@ -1,8 +1,19 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el")
-
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLead();
 });
-console.log(myLeads);
+function renderLead() {
+let listItem = "";
+for(i=0; i<myLeads.length; i++) {
+     listItem += "<li>" + myLeads[i] + "</li>";
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
+}
+ulEl.innerHTML = listItem;
+}
