6509e208228b66424f3a30364dc2f14a3fc76e56
AmanUllah687 <Amanch687@gmail.com>
2024-02-27 20:42:56 +0500

    changes some file Names to index files

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -2,6 +2,8 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+// localStorage.setItem("myleads" , "www.examplelead.com");
+ console.log(localStorage.getItem("myleads"));
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
