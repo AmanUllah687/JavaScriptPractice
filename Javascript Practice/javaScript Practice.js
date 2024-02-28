036f7b2b4a4dfde9f8a1ec85c714db1160d2845a
AmanUllah687 <Amanch687@gmail.com>
2024-02-28 17:19:27 +0500

    updated code

--- Javascript Practice/javaScript Practice.js
+++ Javascript Practice/javaScript Practice.js
@@ -381,5 +381,12 @@ if(currentViewvers) {
     console.log("We have viewvers");
 }
 */
-let trueOfFalse = Boolean("HEllo");
-console.log(trueOfFalse);
+// let trueOfFalse = Boolean("HEllo");
+// console.log(trueOfFalse);
+console.log(Boolean(""));
+console.log(Boolean (0));
+console.log(Boolean(100));
+console.log(Boolean(null));
+console.log([0]);
+console.log(Boolean (0));
+
