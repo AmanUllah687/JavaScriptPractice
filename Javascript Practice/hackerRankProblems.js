e54cd4d1ae197f8a55741bee16cb69ec59f48b02
AmanUllah687 <Amanch687@gmail.com>
2024-03-20 15:57:42 +0500

    Added problem 2 in hackerrank.js file

--- Javascript Practice/hackerRankProblems.js
+++ Javascript Practice/hackerRankProblems.js
@@ -1,4 +1,5 @@
 // hacker rank problem 1
+/*
 let array = [1,2,3];
 let revArray = []
   
@@ -6,3 +7,19 @@ let revArray = []
         revArray.push(array[i])
     }
  console.log(revArray);
+ */
+ // hacker rank problem 2
+let Array = [
+    [10, 20, 60],
+    [8, 10, 52],
+    [15, 5, 24],
+    [26, 28, 43],
+    [12, 16, 51]
+  ];
+  let sum = 0;
+  Array.forEach((row) => {
+    row.forEach((element) => {
+        sum += element;
+    });
+ });
+console.log(sum);
